651c7c437bd28f0a641724265e0c89ace3ce79c4
Claudiu Olteanu <olteanu.claudiu@ymail.com>
2014-05-11 17:54:23 +0300

    Update with server skeleton and new game functionalities

---
+++ Server/src/utils/GameInfo.java
@@ -0,0 +1,26 @@
+package utils;
+
+public class GameInfo {
+	private int roomId;
+	private String playerOne;
+	private String playerTwo;
+	
+	public int getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(int roomId) {
+		this.roomId = roomId;
+	}
+	public String getPlayerOne() {
+		return playerOne;
+	}
+	public void setPlayerOne(String playerOne) {
+		this.playerOne = playerOne;
+	}
+	public String getPlayerTwo() {
+		return playerTwo;
+	}
+	public void setPlayerTwo(String playerTwo) {
+		this.playerTwo = playerTwo;
+	}
+}

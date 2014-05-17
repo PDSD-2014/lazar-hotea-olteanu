eb82ec40799dbf8ab619d0275c9bab7d514ad4c9
Claudiu Olteanu <olteanu.claudiu@ymail.com>
2014-05-17 17:07:09 +0300

    Add a script which generates Server's jar archive

---
+++ Server/create-server-jar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Generates a jar archive which contains all sources needed by Server
+cp config.properties bin
+cp json-simple-1.1.1.jar bin
+cp mysql-connector-java-5.1.29-bin.jar bin
+cd bin
+jar cf server.jar json-simple-1.1.1.jar mysql-connector-java-5.1.29-bin.jar core/*.class db/*.class gameplay/*.class utils/*.class config.properties
